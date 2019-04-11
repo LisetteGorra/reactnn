71a20ad3f1d944793496e184379c0e6a1a84d644
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-04-11 11:56:05 -0400

    Passing a list of ninjas, using .id, looping and outputting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import Ninjas from './nesting';
 
 class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Liz', age: 25, belt: 'black', id: 1},
+      { name: 'Chris', age: 25, belt: 'green', id: 2},
+      { name: 'Revenge', age: 1, belt: 'white', id: 3}
+      
+      ]
+  }
   render() {
     return (
       <div className="App">
          <h1>My first React app!</h1>
          <p>Welcome :B</p>
-         <Ninjas name="Liz" age="25" belt="black"/>
-         <Ninjas name="Chris" age="25" belt="black"/>
+         <Ninjas ninjas={this.state.ninjas}/>
       </div>
     );
   }
