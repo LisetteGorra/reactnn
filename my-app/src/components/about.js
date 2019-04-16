08fdb329b2880690526b2a4e886cafe7360de407
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-04-16 14:29:32 -0400

    Higher order components, used wrappedComponent, spread operator

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import Rainbow from '../hoc/Rainbow';
 
 const About = () => {
-    return(
+    return (
         <div> className="conatiner>
         <h4 className="center">About</h4>
         <p>blah blah blah erihguv ehuerg besgjfehrgvfh gk gher h jkfg ksefg g bheefg grvg jhm</p>
         </div>
-        
-        )
-    }
 
-export default About;
+    )
+}
+
+export default Rainbow(About);
--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
 const Home = () => {
-    return(
+    return (
         <div> className="conatiner>
         <h4 className="center">Home</h4>
         <p>blah blah blah erihguv ehuerg besgjfehrgvfh gk gher h jkfg ksefg g bheefg grvg jhm</p>
         </div>
-        
-        )
-    }
+
+    )
+}
 
 export default Home;
