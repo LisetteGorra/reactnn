d1e7b1fbf87b15ee19533873cc9c7226317d37dd
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-04-17 15:39:38 -0400

    setting up route parameters to output by using id, and url, used post

--- my-app/src/components/posts.js
+++ my-app/src/components/posts.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+
 
 class Post extends Component {
     state = {
-        id: null
+        post: null
     }
     componentDidMount(){
         let id = this.orops.match.params.post_id;
+        axios.get('https://jsonplaceholder.tyicode.com/post' + id)
+        .then(res => {
+            this.setState({
+                post: res.data
+            })
+            
+            
+        })
         this.setState({
             id: id
         })
     }
     render() {
+        
+        const post = this.state.post ? (
+            <div className="post">
+            <h4 className="center">{this.state.post.title}</h4>
+            <p>{this.state.post.body}</p>
+            </div>
+            
+            ) : (
+            <div className="center">loading post</div>
+            )
+        
         return (
             <div className="container">
-            <h4>this.state.id</h4>
+            <h4></h4>
             </div>
         )
     }
