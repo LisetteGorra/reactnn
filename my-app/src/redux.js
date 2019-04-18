a31815c1d8d2dc2099c9fe4f3549f7857058fde0
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-04-18 17:41:41 -0400

    Used dispatch to post and add todos

--- my-app/src/redux.js
+++ my-app/src/redux.js
@@ -8,13 +8,27 @@ const initState = {
 function myreducer(state = initState, action){
     if (action.type == 'ADD_TODO') {
         return {
+            ...state,
             todos: [...state.todos, action.todo]
         }
     }
+    
+    if (action.type == 'ADD_POST') {
+        return {
+            ...state,
+           posts: [...state.todos, action.post]
+        }
+    }
+    
 }
 
 const store = createStore(myreducer);
 
-const todoAction = {type: 'ADD_TODO', todo: 'buy milk'}
+store.subscribe(() => {
+    console.log('state updated');
+    console.log(store.getState());
+});
 
-store.dispatch(todoAction)
+store.dispatch({type: 'ADD_TODO', todo: 'do homework'});
+store.dispatch({type: 'ADD_TODO', todo: 'study'});
+store.dispatch({type: 'ADD_TODO', post: 'create wiki Api'});
